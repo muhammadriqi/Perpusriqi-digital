215ecc26c4e4745ca080ac1c93e410c20c8b9682
muhammadriqi <142365175+muhammadriqi@users.noreply.github.com>
2024-05-20 19:53:41 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,17 +3,16 @@
     <div class="row my-5">
       <div class="col-lg-6">
         <nuxt-link to="/pengunjung/tambah">
-          <div class="card bg-pengunjung rounded-5">
+          <div class="card bg-pengunjung rounded-4" data-v-98588d9b="">
             <div class="card-body">
               <h2>Pengunjung</h2>
             </div>
           </div>
         </nuxt-link>
       </div>
-
       <div class="col-lg-6">
         <nuxt-link to="/buku">
-          <div class="card bg-buku rounded-5">
+          <div class="card bg-buku rounded-4">
             <div class="card-body">
               <h2>Cari Buku</h2>
             </div>
@@ -24,15 +23,17 @@
   </div>.
   <h2 class="justify-content-between">STATISIK</h2>
   <div class="container-fluid">
-    <div class="row justify-content-evenly rounded-5">
+    <div class="row justify-content-evenly rounded-2">
       <div class="col-5">
-        <div class="cover">
-          <h2> 14 Pengunjung </h2>
+        <div class="raccing">
+          <nuxt-link to="pengunjung">
+            <h2>{{ visitors.length }} Pengunjung </h2>
+          </nuxt-link>
         </div>
       </div>
       <div class="col-5">
-        <div class="cover1">
-          <h2> 20 Buku</h2>
+        <div class="raccing1">
+          <h2>{{ books.length }} Buku</h2>
         </div>
       </div>
     </div>
@@ -46,14 +47,14 @@
 }
 
 .card.bg-pengunjung {
-  background-image: url('../assets/img/pengunjung.jpeg');
+  background-image: url(../assets/img/bg-home-kunjungan.jpeg);
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
 }
 
 .card.bg-buku {
-  background: url('../assets/img/bg-home-cari-buku.jpg') no-repeat center center;
+  background-image: url("../assets/img/bg-home-cari-buku.jpg") no-repeat center center;
   background-size: cover;
 }
 
