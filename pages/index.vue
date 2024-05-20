02a3360a2b7e5e1757482df692290e7d776fdf62
muhammadriqi <142365175+muhammadriqi@users.noreply.github.com>
2024-05-20 19:39:37 +0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,16 +3,17 @@
     <div class="row my-5">
       <div class="col-lg-6">
         <nuxt-link to="/pengunjung/tambah">
-          <div class="card bg-pengunjung rounded-4" data-v-98588d9b="">
+          <div class="card bg-pengunjung rounded-5">
             <div class="card-body">
               <h2>Pengunjung</h2>
             </div>
           </div>
         </nuxt-link>
       </div>
+
       <div class="col-lg-6">
         <nuxt-link to="/buku">
-          <div class="card bg-buku rounded-4">
+          <div class="card bg-buku rounded-5">
             <div class="card-body">
               <h2>Cari Buku</h2>
             </div>
@@ -23,17 +24,15 @@
   </div>.
   <h2 class="justify-content-between">STATISIK</h2>
   <div class="container-fluid">
-    <div class="row justify-content-evenly rounded-2">
+    <div class="row justify-content-evenly rounded-5">
       <div class="col-5">
-        <div class="raccing">
-          <nuxt-link to="pengunjung">
-            <h2>{{ visitors.length }} Pengunjung </h2>
-          </nuxt-link>
+        <div class="cover">
+          <h2> 14 Pengunjung </h2>
         </div>
       </div>
       <div class="col-5">
-        <div class="raccing1">
-          <h2>{{ books.length }} Buku</h2>
+        <div class="cover1">
+          <h2> 20 Buku</h2>
         </div>
       </div>
     </div>
@@ -47,18 +46,18 @@
 }
 
 .card.bg-pengunjung {
-  background-image: url(../assets/img/bg-home-kunjungan.jpeg);
+  background-image: url('../assets/img/pengunjung.jpeg');
   background-repeat: no-repeat;
   background-position: center center;
   background-size: cover;
 }
 
 .card.bg-buku {
-  background-image: url("../assets/img/bg-home-cari-buku.jpg") no-repeat center center;
+  background: url('../assets/img/bg-home-cari-buku.jpg') no-repeat center center;
   background-size: cover;
 }
 
-.raccing {
+.cover {
   height: 174px;
   box-shadow: 1px 1px 10px;
   border-radius: 20px;
@@ -68,7 +67,7 @@
   background-color: #BDFF00;
 }
 
-.raccing1 {
+.cover1 {
   height: 174px;
   box-shadow: 1px 1px 10px;
   border-radius: 20px;
@@ -78,26 +77,3 @@
   background-color: #00FFA3;
 }
 </style>
-<script setup>
-const supabase = useSupabaseClient()
-
-const visitors = ref([])
-const books = ref([])
-
-const getPengunjung = async () => {
-  const { data, error } = await supabase.from('pengunjung').select('*, keanggotaan(*), keperluan(*)')
-  if (data) visitors.value = data
-}
-
-const getBooks = async () => {
-  const { data, error } = await supabase.from('buku').select(`*, kategori(*)`)
-  if (data) books.value = data
-}
-
-onMounted (() => {
-  getPengunjung()
-  getBooks()
-})
-
-
-</script>
